a0e3d3d559f1ab7c22f540c5c95dfb5b029f9db0
Rain <rain.kutt@khk.ee>
2019-02-14 13:51:13 +0200

    2.12 tehtud

--- src/AddressBook.js
+++ src/AddressBook.js
@@ -1,2 +1,17 @@
 // loome konstruktor-funktsiooni
-function AddressBook() {}
+function AddressBook() {
+  //lisan uue property klassile, mis on massiv
+  this.contacts = [];
+}
+
+//loome uue funktsioon addContact, mille parameetriks on contact ja lükkab contact-i massiivi, mis on AddressBook klassis
+AddressBook.prototpe.addContact = function(contact) {
+  //siit läheb contact AddressBooki
+  this.contacts.push(contact);
+};
+
+//loon getContact funktsiooni, mis aktsepteerib index-t ja tagastab masiivi index-iga
+AddressBook.prototpe.getContact = function(index) {
+  //siit läheb contact AddressBooki
+  return this.contacts[index];
+};
