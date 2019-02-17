d4a1dd0c195a315461ca44f30ff49b41a8c2467d
Rain <rain.kutt@khk.ee>
2019-02-17 17:53:30 +0200

    2.13 tehtud

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -12,4 +12,21 @@ describe('Address Book', function() {
     // kontrollin, kas kontakt antakse edasi ja ootab integer arve
     expect(addressBook.getContact(0)).toBe(thisContact);
   });
+
+  // defineerin uue spek-i it meetodiga
+  it('should be able to deleta a contact', function() {
+    // lisan uue AadressBook objekti siia spek-i
+    var addressBook = new AddressBook(),
+      // see on uus objekt, mis antakse edasi
+      thisContact = new Contact();
+
+    // lisan kontakti oma addressBook-le
+    addressBook.addContact(thisContact);
+
+    // nüüd saan seda ka kustutada
+    addressBook.deleteContact(0);
+
+    // testimaks oma onbekti
+    expect(addressBook.getContact(0)).not.toBeDefined();
+  });
 });
--- src/AddressBook.js
+++ src/AddressBook.js
@@ -1,17 +1,23 @@
 // loome konstruktor-funktsiooni
 function AddressBook() {
-  //lisan uue property klassile, mis on massiv
+  // lisan uue property klassile, mis on massiv
   this.contacts = [];
 }
 
-//loome uue funktsioon addContact, mille parameetriks on contact ja lükkab contact-i massiivi, mis on AddressBook klassis
+// loome uue funktsioon addContact, mille parameetriks on contact ja lükkab contact-i massiivi, mis on AddressBook klassis
 AddressBook.prototpe.addContact = function(contact) {
-  //siit läheb contact AddressBooki
+  // siit läheb contact AddressBook-i
   this.contacts.push(contact);
 };
 
-//loon getContact funktsiooni, mis aktsepteerib index-t ja tagastab masiivi index-iga
+// loon getContact funktsiooni, mis aktsepteerib index-t ja tagastab masiivi index-iga
 AddressBook.prototpe.getContact = function(index) {
-  //siit läheb contact AddressBooki
+  //s iit läheb contact AddressBook-i
   return this.contacts[index];
+};
+
+// splice meetod on elemendi eemaldamiseks
+AddressBook.prototpe.deleteContact = function(index) {
+  // siit läheb contact AddressBook-i
+  return this.splice(index, 1);
 };
