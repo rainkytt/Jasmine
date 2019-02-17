7ec39b4dc82af3b47c71c06a2636710f255e62f3
Rain <rain.kutt@khk.ee>
2019-02-17 18:23:53 +0200

    2.15 tehtud

--- src/AddressBook.js
+++ src/AddressBook.js
@@ -2,22 +2,38 @@
 function AddressBook() {
   // lisan uue property klassile, mis on massiv
   this.contacts = [];
+  // asünkroonse testimise jaoks
+  this.initialComplete = false;
 }
 
+// loome meetodi getInitialContacts
+AddressBook.prototype.getInitialContacts = function(cb) {
+  var self = this;
+
+  // setTomeout-ga muudame meetodi asünkroonseks
+  setTimeout(function() {
+    // võltsitud APi jaoks kui asünkroonne funktsioon on valmis
+    self.initialComplete = true;
+    if (cb) {
+      return cb();
+    }
+  }, 3);
+};
+
 // loome uue funktsioon addContact, mille parameetriks on contact ja lükkab contact-i massiivi, mis on AddressBook klassis
-AddressBook.prototpe.addContact = function(contact) {
+AddressBook.prototype.addContact = function(contact) {
   // siit läheb contact AddressBook-i
   this.contacts.push(contact);
 };
 
 // loon getContact funktsiooni, mis aktsepteerib index-t ja tagastab masiivi index-iga
-AddressBook.prototpe.getContact = function(index) {
+AddressBook.prototype.getContact = function(index) {
   //s iit läheb contact AddressBook-i
   return this.contacts[index];
 };
 
 // splice meetod on elemendi eemaldamiseks
-AddressBook.prototpe.deleteContact = function(index) {
+AddressBook.prototype.deleteContact = function(index) {
   // siit läheb contact AddressBook-i
   return this.splice(index, 1);
 };
